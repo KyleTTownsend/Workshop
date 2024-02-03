6affbccf102485bf70d53ff3c6fc58b77c850982
KyleTTownsend <KyleTTownsend@gmail.com>
2024-02-03 12:02:42 -0500

    unity files 2

---
+++ LethalCompanyUnityTemplate/Assets/Scripts/Assembly-CSharp/NoisemakerProp.cs
@@ -0,0 +1,39 @@
+using System;
+using UnityEngine;
+
+public class NoisemakerProp : GrabbableObject
+{
+	public AudioSource noiseAudio;
+
+	public AudioSource noiseAudioFar;
+
+	[Space(3f)]
+	public AudioClip[] noiseSFX;
+
+	public AudioClip[] noiseSFXFar;
+
+	[Space(3f)]
+	public float noiseRange;
+
+	public float maxLoudness;
+
+	public float minLoudness;
+
+	public float minPitch;
+
+	public float maxPitch;
+
+	private System.Random noisemakerRandom;
+
+	public Animator triggerAnimator;
+
+	public override void Start()
+	{
+        base.Start();
+    }
+
+    public override void ItemActivate(bool used, bool buttonDown = true)
+    {
+        base.ItemActivate(used, buttonDown);
+    }
+}
